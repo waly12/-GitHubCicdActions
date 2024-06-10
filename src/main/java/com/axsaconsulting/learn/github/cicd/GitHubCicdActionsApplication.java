16d798484cb91b6f96b27d42a1fafc218af7bceb
Axmad-wali XASSAN KAHIN <ahassanahmed@gmail.com>
2024-06-10 13:09:30 +0200

    code

---
+++ src/main/java/com/axsaconsulting/learn/github/cicd/GitHubCicdActionsApplication.java
@@ -0,0 +1,21 @@
+package com.axsaconsulting.learn.github.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitHubCicdActionsApplication {
+	
+	@GetMapping("/welcome")
+	public String welcome() {;
+		return ("Welcome Axsa Consulting first GitHub CI/CD Project..");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubCicdActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axsaconsulting/learn/github/cicd/GitHubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axsaconsulting.learn.github.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
